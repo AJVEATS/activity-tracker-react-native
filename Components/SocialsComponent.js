109951efc2da458ad7b37dc9c07900578167b6fc
Alexander <ajveats@gmail.com>
2022-12-31 12:56:57 +0000

    Styled the welcome screen

---
+++ Components/SocialsComponent.js
@@ -0,0 +1,60 @@
+/**
+ * @fileoverview This file is for SocialsComponent renders pressable icons for our social 
+ * media accounts. Pressing an icon will open that social media in the user's browser
+ */
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { StyleSheet, View, Linking } from 'react-native';
+import colors from '../colors';
+import React from 'react';
+
+const SocialsComponent = () => {
+    return (
+        <View style={styles.socialContainer}>
+            <Ionicons
+                style={styles.icon}
+                name='logo-facebook'
+                onPress={() => {
+                    Linking.openURL('https://www.facebook.com/')
+                }}
+            />
+            <Ionicons
+                style={styles.icon}
+                name='logo-instagram'
+                onPress={() => {
+                    Linking.openURL('https://www.instagram.com/')
+                }}
+            />
+            <Ionicons
+                style={styles.icon}
+                name='logo-twitter'
+                onPress={() => {
+                    Linking.openURL('https://www.twitter.com/')
+                }}
+            />
+            <Ionicons
+                style={styles.icon}
+                name='logo-youtube'
+                onPress={() => {
+                    Linking.openURL('https://www.youtube.com/')
+                }}
+            />
+        </View>
+    )
+}
+
+export default SocialsComponent
+
+const styles = StyleSheet.create({
+    socialContainer: {
+        width: '80%',
+        display: 'flex',
+        flexDirection: 'row',
+        marginVertical: 10,
+        justifyContent: 'space-evenly'
+    },
+    icon: {
+        color: colors.black,
+        marginHorizontal: 5,
+        fontSize: 32,
+    }
+})
--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,28 +1,39 @@
-import { Pressable, StyleSheet, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import colors from '../colors'
+/**
+ * @fileoverview This file is for WelcomeScreen which is the first screen that the user will see 
+ * when they open the app for the first time. There are buttons to navigate to the login for 
+ * existing users and the create account screen for new users. Below the buttons is the 
+ * SocialsComponent for the app's socials  
+ */
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import SocialsComponent from '../Components/SocialsComponent';
+import colors from '../colors';
+import React from 'react';
 
 const WelcomeScreen = ({ navigation }) => {
     return (
-        <SafeAreaView>
-            <Text>WelcomeScreen</Text>
-            <Pressable
-                style={styles.navigateButton}
-                accessibilityLabel='create account button'
-                onPress={() => {
-                    navigation.push('LoginScreen')
-                }} >
-                <Text style={styles.pressableText}>Login</Text>
-            </Pressable>
-            <Pressable
-                style={styles.navigateButton}
-                accessibilityLabel='create account button'
-                onPress={() => {
-                    navigation.push('CreateAccountScreen')
-                }} >
-                <Text style={styles.pressableText}>create account</Text>
-            </Pressable>
+        <SafeAreaView style={styles.welcomeContainer}>
+            <Text style={styles.title}>Activity Tracker</Text>
+            <Text style={styles.subTitle}>Record & View Your Activites</Text>
+            <View style={styles.buttonContainer}>
+                <Pressable
+                    style={styles.navigateButton}
+                    accessibilityLabel='create account button'
+                    onPress={() => {
+                        navigation.push('LoginScreen')
+                    }} >
+                    <Text style={styles.pressableText}>Login</Text>
+                </Pressable>
+                <Pressable
+                    style={styles.navigateButton}
+                    accessibilityLabel='create account button'
+                    onPress={() => {
+                        navigation.push('CreateAccountScreen')
+                    }} >
+                    <Text style={styles.pressableText}>create account</Text>
+                </Pressable>
+            </View>
+            <SocialsComponent />
         </SafeAreaView>
     )
 }
@@ -30,10 +41,29 @@ const WelcomeScreen = ({ navigation }) => {
 export default WelcomeScreen
 
 const styles = StyleSheet.create({
+    welcomeContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    title: {
+        fontSize: 42,
+        marginBottom: 10,
+    },
+    subTitle: {
+        fontSize: 22,
+        marginBottom: 10,
+    },
+    buttonContainer: {
+        width: '85%',
+    },
     navigateButton: {
+        width: '100%',
         backgroundColor: colors.black,
         borderRadius: 4,
         padding: 10,
+        marginTop: 10,
         alignItems: 'center',
     },
     pressableText: {
@@ -41,5 +71,5 @@ const styles = StyleSheet.create({
         fontSize: 17,
         fontWeight: 'bold',
         textTransform: 'uppercase',
-    }
+    },
 })
