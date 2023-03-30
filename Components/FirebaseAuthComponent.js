1ad4713a088cdf99447cf8696081acd776dc9e10
Alex <40860289+AJVEATS@users.noreply.github.com>
2023-03-30 20:37:50 +0100

    Update FirebaseAuthComponent.js

--- Components/FirebaseAuthComponent.js
+++ Components/FirebaseAuthComponent.js
@@ -2,10 +2,5 @@
  * @fileoverview This file represets the the configuration for the app's firebase connection * 
  */
 export const firebaseConfig = {
-    apiKey: "AIzaSyA3dqjY7sjqHRM1GKPNbMdM92I5Nd6UPKw",
-    authDomain: "ubicomp-2-av.firebaseapp.com",
-    projectId: "ubicomp-2-av",
-    storageBucket: "ubicomp-2-av.appspot.com",
-    messagingSenderId: "448485848532",
-    appId: "1:448485848532:web:fdd2ee2cda98422b5c9241"
+    
 };
